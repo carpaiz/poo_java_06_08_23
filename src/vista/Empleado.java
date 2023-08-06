59aa6866d2b6c57a14976042345c90ac259241fb
carpaiz <paiz2790@gmail.com>
2023-08-06 12:31:59 -0600

    primer commit

---
+++ src/vista/Empleado.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vista;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author paiz2
+ */
+public class Empleado extends Persona{
+    private String codigo,puesto;
+
+    public Empleado() {
+    }
+
+    public Empleado(String codigo, String puesto, String nombres, String apellidos, String direccion, String telefono, String fecha_nacimiento) {
+        super(nombres, apellidos, direccion, telefono, fecha_nacimiento);
+        this.codigo = codigo;
+        this.puesto = puesto;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getPuesto() {
+        return puesto;
+    }
+
+    public void setPuesto(String puesto) {
+        this.puesto = puesto;
+    }
+    @Override
+    protected String[] crear(){
+           try
+            {         
+                      String datos[]= new String[7];      
+                      datos[0] = getCodigo();
+                      datos[1] = getNombres();
+                      datos[2] = getApellidos();
+                      datos[3] = getDireccion();
+                      datos[4] = getTelefono();
+                      datos[5] = getFecha_nacimiento();
+                      datos[6] = getPuesto();
+              return  datos;            
+                 }catch(Exception ex){
+                  
+                    JOptionPane.showMessageDialog(null,ex.getMessage(),"Error en Query",JOptionPane.ERROR_MESSAGE);
+                    return  null;
+            }
+           
+           
+   }
+    
+}
